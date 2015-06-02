8980070ff78ae7cb93c455e2e011a9629d9d5f1b
support@afterlogic.com <support@afterlogic.com>
2015-06-02 17:17:28 +0300

    add files

---
+++ conrad-change-login-details/index.php
@@ -0,0 +1,52 @@
+<?php
+
+/* -AFTERLOGIC LICENSE HEADER- */
+
+class_exists('CApi') or die();
+
+class CConradChangeLoginDetailsPlugin extends AApiPlugin
+{
+	/**
+	 * @param CApiPluginManager $oPluginManager
+	 */
+	public function __construct(CApiPluginManager $oPluginManager)
+	{
+		parent::__construct('1.0', $oPluginManager);
+
+		$this->AddHook('api-integrator-login-to-account', 'PluginIntegratorLoginToAccount');
+	}
+
+	private function convertEmailToLogin($sEmail)
+	{
+		$sResult = $sEmail;
+		
+		$iPos = strrpos($sEmail, '.');
+		if (false !== $iPos && 0 < $iPos)
+		{
+			$sResult = substr($sResult, 0, $iPos);
+		}
+		
+		return str_replace('@', '.', $sResult);
+	}
+	
+	/**
+	 * @param string $sEmail
+	 * @param string $sIncPassword
+	 * @param string $sIncLogin
+	 * @param string $sLanguage
+	 * @param bool $bAuthResult
+	 */
+	public function PluginIntegratorLoginToAccount(&$sEmail, &$sPassword, &$sLogin, &$sLanguage, &$bAuthResult)
+	{
+		if (empty($sLogin))
+		{
+			$sLogin = $this->convertEmailToLogin($sEmail);
+		}
+		else
+		{
+			$sLogin = $this->convertEmailToLogin($sLogin);
+		}
+	}
+}
+
+return new CConradChangeLoginDetailsPlugin($this);
